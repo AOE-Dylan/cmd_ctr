1e05a2d3849706c80d2e023f5f455bae0d635409
AOE-Dylan <dylantt2000@gmail.com>
2016-12-14 09:24:14 -1000

    events

---
+++ JS/app.js
@@ -0,0 +1,21 @@
+console.log("hi");
+
+function colorChange() {
+    document.getElementById("box").style.backgroundColor = "red";
+}
+
+function colorBack() {
+    document.getElementById("box").style.backgroundColor = "pink";
+}
+
+function colorChange2() {
+    document.getElementById("box2").style.backgroundColor = "blue";
+}
+
+function colorBack2() {
+    document.getElementById("box2").style.backgroundColor = "orange";
+}
+
+function troll() {
+  document.getElementById("troll").style.visibility = "hidden";
+}
